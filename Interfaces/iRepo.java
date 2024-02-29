f079f0d024e7f2b27092b4c7f191045299d03fd7
Francisco <franciscomarinalvarez@hotmail.es>
2024-02-29 12:37:16 +0100

    Interfaces finished

---
+++ Interfaces/iRepo.java
@@ -0,0 +1,25 @@
+package Interfaces;
+
+import Model.Project;
+import Model.Task;
+import Model.User;
+
+public interface iRepo {
+    User createUser(User userToCreate);
+    User removeUser(User userToRemove);
+    User showUser(String username);
+    User upgradeUser(User userToUpgrade);
+
+    Project createProject(Project projectToCreate);
+    Project removeProject(String projectName);
+    Project showProject(String projectName);
+    Project selectProject(String projectName);
+    Project[] listAllProjects();
+    Project upgradeProject(Project projectToUpgrade, String originalProjectName);
+
+    Task createTask(Task taskToCreate);
+    Task removeTask(String taskName);
+    Task[] listAllTask();
+    Task showTask(String taskName);
+    Task upgradeTask(Task taskToUpgrade,String originalTask);
+}
